7ea3f2b83c28df716b5e3a71acdf44f4c1d97acb
ayameir <ayaa.leon@gmail.com>
2017-10-12 22:38:32 -0400

    Add js for rendering and calculating values.

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-  console.log('this is working!');
-
+  // Click functions.
   $("#click-test").click(function() {
     alert( "Handler for .click() called." );
     console.log('Clicked');
   });
-
   $("#login").click(function() {
     alert("Remember me!");
   });
+  $("#click-here").click(function(){
+    $(this).hide();
+  });
 
-  
+  // Render average values of stuff.
+  var sub1 = 149;
+  var sub2 = 150;
+  var sub3 = 145;
+  var sub4 = 147;
+  var tNum = 4;
+  var avg = (sub1 + sub2 + sub3 + sub4)/tNum;
+  var avgValue = "Heres the value: " + avg;
+  console.log('this is working!');
+
+  $("#temp").append(avgValue);
 });
