e64cc031bb480e00e667ce55027ea4bcc80bae37
ayameir <ayaa.leon@gmail.com>
2017-10-23 23:13:51 -0400

    Add code to ask name, add math function.

--- myscripts.js
+++ myscripts.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
   var tNum = 4;
   var avg = (sub1 + sub2 + sub3 + sub4)/tNum;
   var avgValue = "Heres the value: " + avg;
-  console.log('this is working!');
+  // console.log('this is working!');
 
   $("#temp").append(avgValue);
 
@@ -27,23 +27,46 @@ $(document).ready(function() {
   var engineNumb = 1
   var enginesOper = 10
   while (engineNumb <= enginesOper) {
-    console.log("Engine # " + engineNumb + " is running.");
+    // console.log("Engine # " + engineNumb + " is running.");
     engineData.push("Engine # " + engineNumb + " is running.");
     engineNumb++;
   }
 
-   console.log(engineData);
+  console.log(engineData);
   $("#engines").append(engineData);
 
-  for (var brokenEngine = 11;  brokenEngine <= 15; brokenEngine++){
-    console.log("Engine # " + brokenEngine + " isn't working.");
+  for (var brokenEngine = 11;  brokenEngine <= 15; brokenEngine++) {
+    // console.log("Engine # " + brokenEngine + " isn't working.");
     var brEngines = [];
     brEngines.push("Engine # " + brokenEngine + " isn't working.");
-    console.log(brEngines);
+    // console.log(brEngines);
     $("#brEng").append(brEngines);
   }
+
+  // new stuff
+  var userName = prompt("What's your name?");
+  var nameIscorrect = false;
+  while (nameIscorrect == false) {
+    if (confirm("You entered " + userName + ". Is it correct?")) {
+      alert("welcome " + userName + ".");
+    nameIscorrect = true;
+    } else {
+      userName = prompt("What's your name?");
+    }
+  }
+
+  function squaredSum(a, b){
+    return a * a + b * b;
+  }
+  squaredSum(5, 9);
+
+//  var windowHeight = $(window).height();
+//  console.log(windowHeight);
+  //$('#background-image').height(windowHeight);
 
   // Set the height of the browser window to a variable
   // Subtract the difference of the padding from the window height variable
   // Set the background image div to be the value of the calculation above
+// });
+
 });
